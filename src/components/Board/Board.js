5b8f10fb0758f136279876fb6b5ce04abc839e65
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-08-18 20:11:37 -0500

    single board displays

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import boardShape from '../../helpers/props/boardShape';
 
 class Board extends React.Component {
   static propTypes = {
     board: boardShape.boardShape,
+    setSingleBoard: PropTypes.func.isRequired,
+  }
+
+  singleBoardEvent = (e) => {
+    e.preventDefault();
+    const { board, setSingleBoard } = this.props;
+    setSingleBoard(board.id);
   }
 
   render() {
@@ -13,7 +21,7 @@ class Board extends React.Component {
       <div className="card text-center">
       <div className="card-header"><h5>{board.category}</h5></div>
       <div className="card-body">
-        <button className="btn btn-secondary">View Board Details</button>
+        <button className="btn btn-secondary" onClick={this.singleBoardEvent}>View Board Details</button>
       </div>
       <div className="card-footer text-muted"></div>
     </div>
--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Board from '../Board/Board';
 import authData from '../../helpers/data/authData';
 import boardsData from '../../helpers/data/boardsData';
 
 class BoardContainer extends React.Component {
+  static propTypes = {
+    setSingleBoard: PropTypes.func.isRequired,
+  }
+
   state = {
     boards: [],
   }
@@ -16,7 +21,8 @@ class BoardContainer extends React.Component {
 
   render() {
     const { boards } = this.state;
-    const boardCard = boards.map((board) => <Board key={board.id} board={board}/>);
+    const { setSingleBoard } = this.props;
+    const boardCard = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard}/>);
     return (
       <div className="card-columns">
         {boardCard}
