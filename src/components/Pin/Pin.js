942fd95f6c3d0f16dd96619e74c9bcfc075300f4
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-08-22 09:43:58 -0500

    delete single pin works

--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -1,10 +1,17 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import pinShape from '../../helpers/props/pinShape';
 
 class Pin extends React.Component {
   static propTypes = {
     pin: pinShape.pinShape,
+    deletePin: PropTypes.func.isRequired,
+  }
+
+  deletePinEvent = (e) => {
+    e.preventDefault();
+    const { pin, deletePin } = this.props;
+    deletePin(pin.id);
   }
 
   render() {
@@ -12,9 +19,10 @@ class Pin extends React.Component {
 
     return (
       <div className="card">
-        <img className="card-img-top" src={pin.imgUrl} alt="Card image cap" />
-         <div class="card-body">
-         <h5 class="card-title">{pin.title}</h5>
+        <img className="card-img-top" src={pin.imgUrl} alt="Card cap"/>
+         <div className="card-body">
+         <button className="btn btn-danger" onClick={this.deletePinEvent}>delete pin</button>
+         <h5 className="card-title">{pin.title}</h5>
       </div>
     </div>
     );
